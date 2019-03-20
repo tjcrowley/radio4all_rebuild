6d03cc0a5983cb33ec1bba664a10214dd9c4907e
Darren Mckeeman <darren.mckeeman@gmail.com>
2019-03-20 15:25:59 -0700

    signup and more details

--- radio4all/radio4all/models.py
+++ radio4all/radio4all/models.py
@@ -6,8 +6,10 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import UserManager
+
 from django.utils import timezone
 
 
@@ -223,6 +225,8 @@ class Users(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
 
     class Meta:
