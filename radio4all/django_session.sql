0a7cb0ed3901f81bcf7d3367a187a1b176b6e0e2
Darren Mckeeman <darren.mckeeman@gmail.com>
2019-04-18 19:12:52 -0500

    additional tables required for legacy database and django

--- radio4all/django_session.sql
+++ radio4all/django_session.sql
@@ -1,53 +1,12 @@
--- MySQL dump 10.13  Distrib 5.7.11, for osx10.11 (x86_64)
---
--- Host: localhost    Database: rp
--- ------------------------------------------------------
--- Server version	5.7.11
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `django_session`
---
-
 DROP TABLE IF EXISTS django_session;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE django_session (
   session_key varchar(40) NOT NULL,
   session_data longtext NOT NULL,
   expire_date datetime(6) NOT NULL,
   PRIMARY KEY (session_key),
   KEY django_session_expire_date_a5c62663 (expire_date)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
 
 --
 -- Dumping data for table django_session
 --
-
-LOCK TABLES django_session WRITE;
-/*!40000 ALTER TABLE django_session DISABLE KEYS */;
-INSERT INTO django_session VALUES ('pl2ry1bb1zwl475eluwthvlveqs2sl7p','ZWM4MmYwM2ViNDVlNGVkNTVlYTc4MWUzZTdhNjAwYWVjMTA5MWU2Nzp7Il9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9pZCI6IjEiLCJfYXV0aF91c2VyX2hhc2giOiJjMmQ5MzZlYTg4MGI2NzU2ZmU5YTY0NDFlZTNiODIyOTU0ZTg5OWEzIn0=','2018-12-19 01:05:04.733406');
-/*!40000 ALTER TABLE django_session ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-04-12  8:32:31
